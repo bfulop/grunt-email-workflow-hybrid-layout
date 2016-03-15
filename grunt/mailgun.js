bd3da5b195d6d2b266d0d3a13d0e091a9a3e7094
Balint Fulop <bfulop@mac.com>
2016-03-15 12:11:49 +0100

    remove gmail blue links

--- grunt/mailgun.js
+++ grunt/mailgun.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         key: '<%= secrets.mailgun.api_key %>', // See README for secrets.json or replace this with your own key
         sender: '<%= secrets.mailgun.sender %>', // See README for secrets.json or replace this with your preferred sender
         recipient: '<%= secrets.mailgun.recipient %>', // See README for secrets.json or replace this with your preferred recipient
-        subject: 'Responsive buttons'
+        subject: 'Peugeot ODL classique'
       },
       src: ['<%= paths.dist %>/'+grunt.option('template')]
     }
