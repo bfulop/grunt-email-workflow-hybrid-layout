650149d69932550afe89eb380ed5a18bc16746c3
Balint Fulop <bfulop@mac.com>
2016-03-26 21:18:38 +0100

    Helper wraps in centered table

--- src/helpers/helloworld.js
+++ src/helpers/helloworld.js
@@ -1,3 +1,43 @@
-module.exports.helloworld = function (str)  {
-    return "<h1>" + str + "</h1>";
+module.exports.register = function (Handlebars, options)  {
+    Handlebars.registerHelper('foo', function (str)  {
+        var textresult = str + "\n\n\n";
+            for (var aprop in options) {
+            textresult += "\n\n" + aprop + " = " + options[aprop];
+        }
+        return new Handlebars.SafeString(
+
+            '<div class="mybold">' +
+                textresult
+            + '</div>');
+    });
+
+    Handlebars.registerHelper('wrapper', function(str){
+        return new Handlebars.SafeString(
+            `<p> ${str.fn(this)} </p>`
+        );
+    });
+
+    Handlebars.registerHelper('center-wrapper', function(str){
+        return new Handlebars.SafeString(
+            `<center class="wrapper">
+    <div class="webkit">
+        <!--[if (gte mso 9)|(IE)]>
+        <table width="{{ parameters.pagewidth }}" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+                <td>
+        <![endif]-->
+        <table width="{{ parameters.pagewidth }}" class="outer" align="center" cellpadding="0" cellspacing="0">
+
+            ${str.fn(this)}
+
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+    </div>
+</center>`
+        );
+    });
 };
