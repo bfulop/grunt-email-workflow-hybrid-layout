3a0e64cb31a0318a54a091734c40dcb7fb89373e
Balint Fulop <bfulop@mac.com>
2016-04-11 18:07:17 +0200

    Outlook.com middletext images OK

--- grunt/mailgun.js
+++ grunt/mailgun.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         key: '<%= secrets.mailgun.api_key %>', // See README for secrets.json or replace this with your own key
         sender: '<%= secrets.mailgun.sender %>', // See README for secrets.json or replace this with your preferred sender
         recipient: '<%= secrets.mailgun.recipient %>', // See README for secrets.json or replace this with your preferred recipient
-        subject: 'same image everywhere'
+        subject: 'pdv left use background attribute'
       },
       src: ['<%= paths.dist %>/'+grunt.option('template')]
     }
